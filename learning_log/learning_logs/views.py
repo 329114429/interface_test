919e0f1034050b07c2999209564aacfde27f0498
hao <329114429@qq.com>
2020-02-28 15:07:40 +0800

    new_topic

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, HttpResponse, reverse
 from .models import Topic
+from django.http import HttpResponseRedirect
+from .forms import TopicFrom
 
 
 # Create your views here.
@@ -18,7 +20,23 @@ def topics(request):
 
 def topic(request, topic_id):
     """显示单个页面主题"""
-    topic = Topic.objects.get(id=topic_id)
+    topic = Topic.objects.get(id=topic_id)  # 这个id是给topics.html
     entries = topic.entry_set.order_by('-date_add')
     context = {'topic': topic, 'entries': entries}
     return render(request, 'learning_logs/topic.html', context)
+
+
+def new_topic(request):
+    """显示新添加的主题"""
+    if request.method != "POST":
+        # 未提交数据,创建一个新表单
+        form = TopicFrom()
+    else:
+        form = TopicFrom(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
