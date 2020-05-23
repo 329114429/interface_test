6d2588fc6bbae50fbd9b6b90cef1ace16a137142
hao <329114429@qq.com>
2020-05-23 21:40:30 +0800

    card

--- credit/card/admin.py
+++ credit/card/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Card
+
+
+class CreditCard(admin.ModelAdmin):
+    # 需要显示的字段信息
+    list_display = ("name", "expiration_time", "repayment_time", "repay_money")
+
+    # 设置哪些字段可以点击进入编辑界面，默认是第一个字段
+    list_display_links = ("name", "expiration_time")
+
+
+## 注册时，在第二个参数写上 admin model
+admin.site.register(Card, CreditCard)
--- credit/card/models.py
+++ credit/card/models.py
@@ -7,7 +7,30 @@ from django.db import models
 class Card(models.Model):
     # 信用卡的字段信息
 
-    name = models.CharField(max_length=128, null=True, verbose_name="信用卡名字")
-    expiration_time = models.DateTimeField(null=True, verbose_name="到期日期")
-    repayment_time = models.DateTimeField(null=True, verbose_name="还款日期")
-    repay_money = models.FloatField(null=True, verbose_name="还款金额")
+    choice = (
+        (u"0", u"中信信用卡"),
+        (u"1", u"广发信用卡"),
+        (u"2", u"交通信用卡"),
+        (u"3", u"平安信用卡"),
+        (u"4", u"浦发信用卡"),
+        (u"12", u"建设信用卡"),
+        (u"5", u"微信借钱"),
+        (u"6", u"美团借钱"),
+        (u"7", u"京东白条"),
+        (u"8", u"京东金条"),
+        (u"9", u"小米借钱"),
+        (u"10", u"借呗"),
+        (u"11", u"花呗"),
+    )
+
+    name = models.CharField(max_length=128, choices=choice, null=True, blank=True,
+                            verbose_name="信用卡名字")
+    expiration_time = models.DateField(null=True, blank=True, verbose_name="到期日期")
+    repayment_time = models.DateField(null=True, blank=True, verbose_name="还款日期")
+    repay_money = models.FloatField(null=True, blank=True, verbose_name="还款金额")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = verbose_name_plural = "信用卡记录"
