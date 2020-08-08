075bb30d1ccf358ee15d237231fb9e54843553ce
hao <329114429@qq.com>
2020-08-08 18:04:36 +0800

    0808

---
+++ mysite/polls/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, re_path
+
+from . import views
+
+urlpatterns = [
+    re_path('', views.index, name='index'),
+    re_path('<int:question_id>', views.detail, name='detail'),
+    re_path('<int:question_id>/results/', views.results, name='results'),
+    re_path('<int:question_id>/vote/', views.vote, name='vote'),
+]
+
+
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse("hao suo zhong")
+
+
+def detail(request, question_id):
+    response = "you are looking at question %s "
+    return HttpResponse(response % question_id)
+
+
+def results(request, question_id):
+    response = "you are looking at results of question %s"
+    return HttpResponse(response % question_id)
+
+
+def vote(request, question_id):
+    return HttpResponse("you are voting on question %s" % question_id)
+
+
+
