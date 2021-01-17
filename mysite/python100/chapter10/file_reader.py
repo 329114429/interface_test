c304079f5ebccb126562835743aa7b4e6fdc7733
hao <329114429@qq.com>
2021-01-17 23:58:33 +0800

    chapter11

--- mysite/python100/chapter10/file_reader.py
+++ mysite/python100/chapter10/file_reader.py
@@ -1,3 +1,6 @@
+import json
+
+
 # file_path = "/Users/hao/PycharmProjects/mysite/python100/python源码/chapter_10/pi_million_digits.txt"
 
 # with open(file_path) as file:
@@ -55,11 +58,103 @@
 #     else:
 #         print(answer)
 
-filename = "hao.txt"
+# filename = "hao.txt"
+#
+# try:
+#     with open(filename) as file:
+#         contents = file.read()
+# except FileNotFoundError:
+#     msg = "not file"
+#     print(msg)
 
-try:
-    with open(filename) as file:
-        contents = file.read()
-except FileNotFoundError:
-    msg = "not file"
-    print(msg)
+# filename = "/Users/hao/PycharmProjects/mysite/python100/python源码/chapter_10/alice.txt"
+
+
+# def count_words(filename):
+#     try:
+#         with open(filename) as f:
+#             contents = f.read()
+#     except FileNotFoundError:
+#         # msg = "not exits "
+#         # print(msg)
+#         pass
+#     else:
+#         words = contents.split()
+#         number_words = len(words)
+#         print(number_words)
+#
+#
+# filename = ["/Users/hao/PycharmProjects/mysite/python100/python源码/chapter_10/alice.txt", "hao",
+#             "/Users/hao/PycharmProjects/mysite/python100/python源码/chapter_10/little_women.txt"]
+# for file in filename:
+#     count_words(file)
+
+# numbers = [1, 2, 3, 4, 5]
+# filename = 'numbers.json'
+# with open(filename, 'w') as f:
+#     json.dump(numbers, f)
+
+
+# filename = "numbers.json"
+# with open(filename, 'r') as file:
+#     numbers = json.load(file)
+#
+# print(numbers)
+
+# # username = input("your name:")
+# filename = "username.json"
+#
+# # try:
+# #     with open(filename, 'w') as file:
+# #         json.dump(username, file)  # 把username写进file
+# # except FileNotFoundError:
+# #     pass
+# # else:
+# #     print("come back : " + username)
+#
+#
+# try:
+#     with open(filename) as file:
+#         username = json.load(file)
+# except FileNotFoundError:
+#     username = input("your name:")
+#     with open(filename, "w") as file:   # 如果没有，就重新写进入
+#         json.dump(username, file)
+#         print("back" + username)
+# else:
+#     print("back " + username)
+
+
+def get_stored_username():
+    """获取用户"""
+    filename = "/Users/hao/PycharmProjects/mysite/python100/chapter10/username.json"
+    try:
+        with open(filename) as filename:
+            username = json.load(filename)
+    except FileNotFoundError:
+        return None
+    else:
+        return username
+
+
+def get_new_username():
+    """获取新用户"""
+    username = input("your name:")
+    filename = "username.json"
+    with open(filename, 'w') as filename:
+        json.dump(username, filename)
+    return username
+
+
+def greet_user():
+    """问候用户"""
+    username = get_stored_username()
+    if username:
+        print("welcome back :" + username)
+    else:
+        username = get_new_username()
+        print("remember your " + username)
+
+
+if __name__ == '__main__':
+    greet_user()
