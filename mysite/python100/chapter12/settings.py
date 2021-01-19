4a6968f0da45ad7b570acb5953bb74c24359d3af
hao <329114429@qq.com>
2021-01-20 00:20:40 +0800

    alien_invasion

---
+++ mysite/python100/chapter12/settings.py
@@ -0,0 +1,10 @@
+class Settings():
+    # 存储外星人入侵的所有设置类
+
+    def __init__(self):
+        # 初始化设置类
+
+        # 屏幕设置
+        self.screen_width = 1142
+        self.screen_height = 630
+        self.bg_color = (230, 230, 230)
