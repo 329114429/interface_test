08f104a99eed8b86d4a60096385d0ec75dcabb99
hao <hao@192.168.0.102>
2021-02-04 00:21:55 +0800

    rw_visual

---
+++ mysite/python100/chapter15/rw_visual.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+
+from python100.chapter15.random_walk import RandomWalk
+
+# 创建一个RandomWalk实例
+while True:
+    rw = RandomWalk()
+    rw.fill_walk()
+    plt.scatter(rw.x_values, rw.y_values, s=15)
+    plt.show()
+
+    keep_running = input("Make another walk y/n : ")
+    if keep_running == "n":
+        break
