787372c833d50ba013b41ea222c925c4cfc25fd4
hao <hao@192.168.0.101>
2021-02-05 22:52:50 +0800

    chapter15

--- mysite/python100/chapter15/rw_visual.py
+++ mysite/python100/chapter15/rw_visual.py
@@ -1,3 +1,6 @@
+import sys
+sys.path.append(".")
+
 from python100.chapter15.random_walk import RandomWalk
 import matplotlib.pyplot as plt
 
@@ -10,11 +13,20 @@ while True:
     plt.figure(dpi=128, figsize=(10, 6))
 
     point_numbers = list(range(rw.num_points))
-    plt.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=10)
+    plt.scatter(rw.x_values,
+                rw.y_values,
+                c=point_numbers,
+                cmap=plt.cm.Blues,
+                edgecolors='none',
+                s=10)
 
     # 突出起点和终点
     plt.scatter(0, 0, c='green', edgecolors='none', s=100)
-    plt.scatter(rw.x_values[-1], rw.y_values[-1], c='red', edgecolors='none', s=100)
+    plt.scatter(rw.x_values[-1],
+                rw.y_values[-1],
+                c='red',
+                edgecolors='none',
+                s=100)
 
     # 隐藏坐标轴, 会警告，绘制图之前定义了标题和刻度
     current_axes = plt.axes()
