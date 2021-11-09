66667a4e863f0f7148e52d4cc83f1451b120363f
329114429 <329114429@qq.com>
2021-11-09 17:29:08 +0800

    1109

--- fluent_python/chapter6/demoorder.py
+++ fluent_python/chapter6/demoorder.py
@@ -34,3 +34,42 @@ class Order():
         else:
             discount = self.promotion.discount(self)
         return self.total() - discount
+
+    def __repr__(self):
+        fmt = '<order total:{:2f} due:{:.2f}>'
+        return fmt.format(self.total(), self.due())
+
+
+# 策略，抽奖基类
+class Promotion(ABC):
+    @abstractmethod
+    def discount(self, order):
+        """返回折扣的金额"""
+
+
+# 第一个具体的策略
+class FidelityPromo(Promotion):
+    """积分为1000或以上的享受5%折扣"""
+    def discount(self, order):
+        return order.total() * 0.05 if order.customer.fidelity >= 1000 else 0
+
+
+# 第二个具体策略
+class BulkItemPromo(Promotion):
+    """单个商品为20个或以上提供10%折扣"""
+    def discount(self, order):
+        discount = 0
+        for item in order.cart:
+            if item.quantity >= 20:
+                discount += item.total() * 0.1
+        return discount
+
+
+# 第三个具体策略
+class LargeOrderPromo(Promotion):
+    """订单中的不同商品达到10个或以上提供7%折扣"""
+    def discount(self, order):
+        distinct_items = {item.product for item in order.cart}
+        if len(distinct_items) >= 10:
+            return order.total() * 0.07
+        return 0
